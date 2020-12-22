d2a784170be66802e3c9228045f8638a1ace972d
Ia Lundell <olivia.solumlundell@gmail.com>
2020-12-22 23:45:00 +0100

    Design + regexpill

---
+++ Labb5 - Lundell/ImageScraper.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Labb5___Lundell
+{
+    partial class ImageScraper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExtractBtn = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.Linkbox = new System.Windows.Forms.TextBox();
+            this.ImagesBox = new System.Windows.Forms.TextBox();
+            this.LinkLabel = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.ImgCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ExtractBtn
+            // 
+            this.ExtractBtn.Location = new System.Drawing.Point(594, 78);
+            this.ExtractBtn.Name = "ExtractBtn";
+            this.ExtractBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExtractBtn.TabIndex = 0;
+            this.ExtractBtn.Text = "Extract";
+            this.ExtractBtn.UseVisualStyleBackColor = true;
+            this.ExtractBtn.Click += new System.EventHandler(this.ExtractBtn_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(594, 396);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaveBtn.TabIndex = 1;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // Linkbox
+            // 
+            this.Linkbox.Location = new System.Drawing.Point(23, 50);
+            this.Linkbox.Name = "Linkbox";
+            this.Linkbox.Size = new System.Drawing.Size(646, 22);
+            this.Linkbox.TabIndex = 2;
+            // 
+            // ImagesBox
+            // 
+            this.ImagesBox.Location = new System.Drawing.Point(23, 118);
+            this.ImagesBox.Multiline = true;
+            this.ImagesBox.Name = "ImagesBox";
+            this.ImagesBox.ReadOnly = true;
+            this.ImagesBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ImagesBox.Size = new System.Drawing.Size(646, 272);
+            this.ImagesBox.TabIndex = 3;
+            // 
+            // LinkLabel
+            // 
+            this.LinkLabel.AutoSize = true;
+            this.LinkLabel.Location = new System.Drawing.Point(23, 27);
+            this.LinkLabel.Name = "LinkLabel";
+            this.LinkLabel.Size = new System.Drawing.Size(341, 17);
+            this.LinkLabel.TabIndex = 4;
+            this.LinkLabel.Text = "Insert link to the page you want to scrape for images:";
+            // 
+            // ImgCount
+            // 
+            this.ImgCount.AutoSize = true;
+            this.ImgCount.Location = new System.Drawing.Point(20, 396);
+            this.ImgCount.Name = "ImgCount";
+            this.ImgCount.Size = new System.Drawing.Size(0, 17);
+            this.ImgCount.TabIndex = 5;
+            // 
+            // ImageScraper
+            // 
+            this.AcceptButton = this.ExtractBtn;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(695, 450);
+            this.Controls.Add(this.ImgCount);
+            this.Controls.Add(this.LinkLabel);
+            this.Controls.Add(this.ImagesBox);
+            this.Controls.Add(this.Linkbox);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.ExtractBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ImageScraper";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExtractBtn;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.TextBox Linkbox;
+        private System.Windows.Forms.TextBox ImagesBox;
+        private System.Windows.Forms.Label LinkLabel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label ImgCount;
+    }
+}
+
---
+++ Labb5 - Lundell/ImageScraper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labb5___Lundell
+{
+    public partial class ImageScraper : Form
+    {
+        long img = 0;
+        public ImageScraper()
+        {
+            InitializeComponent();
+        }
+
+        private void ExtractBtn_Click(object sender, EventArgs e)
+        {
+            string pattern = "<img.*src=\"(.*?)\"";
+            string pattern2 = @"(?<=src="")(.*?)(?="")";
+            /*När man trycker på knappen (eller enter) så laddar programmet
+             ner HTML-koden från den URL man matat in, söker igenom denna efter länkar till
+            bilder och visar alla länkar den hittar i en multiline TextBox. Det ska även finnas en
+            Label som visar hur många bild-länkar som hittades på sidan.*/
+            var UserLink = Linkbox.Text;
+            using (WebClient client = new WebClient())
+            {
+                //< img class=”myclass” src=”/example-image.png”><img class=”myclass” src=”/example-image.png”> ---ExempelHTML
+                string htmlCode = client.DownloadString(UserLink);
+                Regex rgx = new Regex(pattern);
+                Regex rgx2 = new Regex(pattern2);
+
+                foreach (Match match in rgx.Matches(htmlCode))
+                {
+                    string match1 = match.ToString();
+                    foreach (Match match2 in rgx.Matches(match1))
+                    {
+                        ImagesBox.AppendText($"{match2.Value}");
+                    }                                     
+                    img++;
+                }
+                     
+                if (img == 0)
+                {
+                    ImgCount.Text = $"We could not find any images in your quest for image domination";
+                }
+                if (img == 1)
+                {
+                    ImgCount.Text = $"{img} image found in your quest for image domination";
+                }
+                else
+                {
+                    ImgCount.Text = $"{img} images found in your quest for image domination";
+                }
+               
+            }
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+          /*Sedan ska det finnas en knapp ”Save Images” som öppnar en
+            -FolderBrowserDialog där man kan välja en mapp dit man vill spara alla bilderna.
+            När man valt en mapp (och klickat ”OK”) så ska programmet asynkront hämta alla
+            bilder via länkarna och spara ner till filer i den ordning nedladdningarna blir klara.
+            Detta görs genom att skapa en ny Task för varje nedladdning och sedan använda
+            Task.WhenAny() för att hantera varje Task när den blir klar. När en Task är klar tar
+            ni det binärdata som den laddat ner från länken och skriver rakt av till en binär fil.
+            Ni kan döpa filerna t.ex Image1, Image2 etc. men tänk på att filändelsen måste
+            matcha datat för att filen ska gå att öppna. Så om länken pekar på t.ex en .png fil
+            så måste filen heta t.ex Image5.png*/
+            saveFileDialog1.ShowDialog();
+            
+        }
+    }
+}
